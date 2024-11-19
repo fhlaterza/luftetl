7676a027b8f51beebdb41ed91a8377732b5fd493
Fernando Laterza <111194951+fhlaterza@users.noreply.github.com>
2024-11-19 16:23:31 -0300

    finalizado

--- obj/Debug/net8.0/luftetl.AssemblyInfo.cs
+++ obj/Debug/net8.0/luftetl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("luftetlC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+894cd58ca7cc24e2307da1e8ea06dba38eaeed3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0585457849c7c5c82222fbdbb082216bd29dbe1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("luftetlC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("luftetlC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
